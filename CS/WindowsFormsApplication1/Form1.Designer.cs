0f1c9ad2c3eb1ccf884c73ba20d15322f40518d4
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsApplication1 {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.spreadsheetControl1 = new DevExpress.XtraSpreadsheet.SpreadsheetControl();
+            this.splitContainerControl1 = new DevExpress.XtraEditors.SplitContainerControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).BeginInit();
+            this.splitContainerControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // spreadsheetControl1
+            // 
+            this.spreadsheetControl1.AllowDrop = true;
+            this.spreadsheetControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.spreadsheetControl1.Location = new System.Drawing.Point(0, 0);
+            this.spreadsheetControl1.Name = "spreadsheetControl1";
+            this.spreadsheetControl1.Options.Export.Csv.Culture = new System.Globalization.CultureInfo("");
+            this.spreadsheetControl1.Options.Export.Txt.Culture = new System.Globalization.CultureInfo("");
+            this.spreadsheetControl1.Options.Export.Txt.ValueSeparator = ',';
+            this.spreadsheetControl1.Options.Import.Csv.Culture = new System.Globalization.CultureInfo("");
+            this.spreadsheetControl1.Options.Import.ThrowExceptionOnInvalidDocument = false;
+            this.spreadsheetControl1.Options.Import.Txt.Culture = new System.Globalization.CultureInfo("");
+            this.spreadsheetControl1.Size = new System.Drawing.Size(605, 476);
+            this.spreadsheetControl1.TabIndex = 0;
+            this.spreadsheetControl1.Text = "spreadsheetControl1";
+            // 
+            // splitContainerControl1
+            // 
+            this.splitContainerControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerControl1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerControl1.Name = "splitContainerControl1";
+            this.splitContainerControl1.Panel1.Controls.Add(this.button3);
+            this.splitContainerControl1.Panel1.Controls.Add(this.button2);
+            this.splitContainerControl1.Panel1.Controls.Add(this.button1);
+            this.splitContainerControl1.Panel1.Text = "Panel1";
+            this.splitContainerControl1.Panel2.Controls.Add(this.spreadsheetControl1);
+            this.splitContainerControl1.Panel2.Text = "Panel2";
+            this.splitContainerControl1.Size = new System.Drawing.Size(724, 476);
+            this.splitContainerControl1.SplitterPosition = 114;
+            this.splitContainerControl1.TabIndex = 1;
+            this.splitContainerControl1.Text = "splitContainerControl1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Import DateTable";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Import Arrays";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Import List";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 476);
+            this.Controls.Add(this.splitContainerControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).EndInit();
+            this.splitContainerControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraSpreadsheet.SpreadsheetControl spreadsheetControl1;
+        private DevExpress.XtraEditors.SplitContainerControl splitContainerControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ CS/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using DevExpress.Spreadsheet;
+using System.Collections.Generic;
+
+namespace WindowsFormsApplication1 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            spreadsheetControl1.Document.BeginUpdate();
+            try {
+                Worksheet worksheet = spreadsheetControl1.Document.Worksheets[0];
+                worksheet.DeleteCells(worksheet.Cells, DeleteMode.ShiftCellsLeft);
+                ImportDataTable();
+                CreateTable();
+            }
+            finally {
+                spreadsheetControl1.Document.EndUpdate();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            spreadsheetControl1.Document.BeginUpdate();
+            try {
+                Worksheet worksheet = spreadsheetControl1.Document.Worksheets[0];
+                worksheet.Clear(worksheet.GetUsedRange());
+                worksheet.Cells["A1"].ColumnWidthInCharacters = 35;
+                worksheet.Cells["A1"].Value = "Import an array horizontally:";
+                worksheet.Cells["A3"].Value = "Import a two-dimensional array:";
+                ImportArrays();
+            }
+            finally {
+                spreadsheetControl1.Document.EndUpdate();
+            }   
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            spreadsheetControl1.Document.BeginUpdate();
+            try {
+                Worksheet worksheet = spreadsheetControl1.Document.Worksheets[0];
+                worksheet.Clear(worksheet.GetUsedRange());
+                worksheet.Cells["A1"].ColumnWidthInCharacters = 35;
+                worksheet.Cells["A1"].Value = "Import data from List vertically:";
+                ImportList();
+            }
+            finally {
+                spreadsheetControl1.Document.EndUpdate();
+            }
+        }
+
+        #region #ImportDataTable
+        void ImportDataTable() {
+
+            Worksheet worksheet = spreadsheetControl1.Document.Worksheets[0];
+
+            // Create a "Products" DataTable object with four columns.
+            DataTable sourceTable = new DataTable("Products");
+            sourceTable.Columns.Add("Product", typeof(string));
+            sourceTable.Columns.Add("Price", typeof(float));
+            sourceTable.Columns.Add("Quantity", typeof(Int32));
+            sourceTable.Columns.Add("Discount", typeof(float));
+
+            sourceTable.Rows.Add("Chocolade", 5, 15, 0.03);
+            sourceTable.Rows.Add("Konbu", 9, 55, 0.1);
+            sourceTable.Rows.Add("Geitost", 15, 70, 0.07);
+
+            // Import data from the data table into the worksheet and insert it, starting with the B2 cell.
+            worksheet.Import(sourceTable, true, 1, 1);
+        }
+        #endregion #ImportDataTable
+
+        #region #CreateTable
+        void CreateTable() {
+
+            Worksheet worksheet = spreadsheetControl1.Document.Worksheets[0];
+
+            // Insert a table in the worksheet.
+            Table table = worksheet.Tables.Add(worksheet["B2:F5"], true);
+
+            // Format the table by applying a built-in table style.
+            table.Style = spreadsheetControl1.Document.TableStyles[BuiltInTableStyleId.TableStyleMedium27];
+
+            // Access table columns.
+            TableColumn productColumn = table.Columns[0];
+            TableColumn priceColumn = table.Columns[1];
+            TableColumn quantityColumn = table.Columns[2];
+            TableColumn discountColumn = table.Columns[3];
+            TableColumn amountColumn = table.Columns[4];
+
+            // Set the name of the last column. 
+            amountColumn.Name = "Amount";
+
+            // Set the formula to calculate the amount per product 
+            // and display results in the "Amount" column.
+            amountColumn.Formula = "=[Price]*[Quantity]*(1-[Discount])";
+
+            // Display the total row in the table.
+            table.ShowTotals = true;
+
+            // Set the label and function to display the sum of the "Amount" column.
+            discountColumn.TotalRowLabel = "Total:";
+            amountColumn.TotalRowFunction = TotalRowFunction.Sum;
+
+            // Specify the number format for each column.
+            priceColumn.DataRange.NumberFormat = "$#,##0.00";
+            discountColumn.DataRange.NumberFormat = "0.0%";
+            amountColumn.Range.NumberFormat = "$#,##0.00;$#,##0.00;\"\";@";
+
+            // Specify horizontal alignment for header and total rows of the table.
+            table.HeaderRowRange.Alignment.Horizontal = SpreadsheetHorizontalAlignment.Center;
+            table.TotalRowRange.Alignment.Horizontal = SpreadsheetHorizontalAlignment.Center;
+
+            // Specify horizontal alignment to display data in all columns except the first one.
+            for (int i = 1; i < table.Columns.Count; i++) {
+                table.Columns[i].DataRange.Alignment.Horizontal = SpreadsheetHorizontalAlignment.Center;
+            }
+
+            // Set the width of table columns.
+            table.Range.ColumnWidthInCharacters = 10;
+        }
+        #endregion #CreateTable
+
+        #region #ImportArrays
+        void ImportArrays() {
+
+            Worksheet worksheet = spreadsheetControl1.Document.Worksheets[0];
+
+            // Create an array containing string values.
+            string[] array = new string[] { "AAA", "BBB", "CCC", "DDD" };
+
+            // Import the array into the worksheet and insert it horizontally, starting with the B1 cell.
+            worksheet.Import(array, 0, 1, false);
+
+            // Create a two-dimensional array containing string values.
+            String[,] names = new String[2, 4]{
+            {"Ann", "Edward", "Angela", "Alex"},
+            {"Rachel", "Bruce", "Barbara", "George"}
+                 };
+
+            // Import the two-dimensional array into the worksheet and insert it, starting with the B3 cell.
+            worksheet.Import(names, 2, 1);
+        }
+        #endregion #ImportArrays
+
+        #region #ImportList
+        void ImportList() {
+
+            Worksheet worksheet = spreadsheetControl1.Document.Worksheets[0];
+
+            // Create a List object containing string values.
+            List<string> cities = new List<string>();
+            cities.Add("New York");
+            cities.Add("Rome");
+            cities.Add("Beijing");
+            cities.Add("Delhi");
+
+            // Import the list into the worksheet and insert it vertically, starting with the B1 cell.
+            worksheet.Import(cities, 0, 1, true);
+        }
+        #endregion #ImportList
+    }
+}
